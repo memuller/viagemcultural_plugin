40a476e0fa74a160fe522c10ed421f521105385f
Matheus E. Muller <hello@memuller.com>
2014-07-24 22:06:12 -0300

    Bugfixes Region migration.

--- base/Base.php
+++ base/Base.php
@@ -40,6 +40,13 @@
 		}
 
 		static function migrate_region_defaults(){
+			global $wpdb; 
+			$wpdb->query("DELETE t.*,tt.*,tr.*
+				FROM $wpdb->terms AS t
+				LEFT JOIN $wpdb->term_taxonomy as tt ON tt.term_id = t.term_id
+				LEFT JOIN $wpdb->term_relationships tr ON tr.term_taxonomy_id = tt.term_taxonomy_id
+				WHERE tt.taxonomy = 'region'"
+			);
 			$taxes = array(
 				'Nacional' => array('Centro-Oeste', 'Nordeste', 'Norte', 'Sul', 'Sudeste'),
 				'Internacional' => array('África', 'América', 'Ásia', 'Europa', 'Oceania')
