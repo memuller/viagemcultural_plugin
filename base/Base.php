2726de56863634cdf90a2ae48a58d84e6914c9ab
Matheus E. Muller <hello@memuller.com>
2014-08-10 16:48:03 -0300

    Removes old drupal migrations.

--- base/Base.php
+++ base/Base.php
@@ -18,10 +18,6 @@
 
 		static $migrations = array(
 			'0.1' => 'ayvp_settings',
-			'0.2' => 'post_type_field_size',
-			'0.3' => 'drupal_database',
-			'0.4' => 'drupal_content',
-			'0.5' => 'drupal_post_types',
 			'0.6' => 'region_defaults'
 
 		);
@@ -51,53 +47,6 @@
 				'option_value' => 'a:15:{s:7:"publish";s:1:"0";s:12:"display_meta";s:1:"0";s:5:"words";s:2:"20";s:4:"dims";a:2:{i:0;s:3:"506";i:1;s:3:"304";}s:7:"related";s:1:"0";s:6:"inlist";s:1:"0";s:4:"cron";s:1:"6";s:4:"user";s:0:"";s:8:"channels";a:2:{i:1;a:6:{s:2:"id";i:1;s:4:"name";s:21:"Canal Viagem Cultural";s:7:"channel";s:18:"progviagemcultural";s:4:"type";s:7:"channel";s:10:"categories";a:1:{i:0;s:1:"1";}s:6:"author";s:1:"1";}i:0;a:1:{s:7:"channel";s:18:"progviagemcultural";}}s:3:"rss";i:0;s:5:"limit";i:4;s:5:"pages";i:0;s:11:"last_import";i:1406047847;s:12:"is_importing";b:0;s:7:"version";i:206;}',
 				'autoload' => 'yes'
 			));
-		}
-
-		static function migrate_post_type_field_size(){
-			global $wpdb; 
-			$wpdb->query("ALTER TABLE $wpdb->posts MODIFY post_type VARCHAR(50)");
-		}
-		static function migrate_drupal_database(){
-			return true;
-			global $wpdb; 
-			ini_set('memory_limit','256M');
-			$query = '';
-			$file = new SplFileObject(static::path('/data/drupal.sql'));
-			while(! $file->eof()){
-				$line = $file->current();
-				if(substr($line, 0, 2) == '--' || $line == ''){ continue; }
-				$query .= $line ;					
-				if(substr(trim($line), -1, 1) == ';'){
-					$wpdb->query($query);
-					$query = '';
-				}
-				$file->next();
-			}
-			$file = null ; 
-
-		}
-		static function migrate_drupal_content(){
-			return true;
-			global $wpdb;
-			ini_set('memory_limit', '256M');
-			$query = '';
-			$file = new SplFileObject(static::path('/data/drupal-to-wordpress.sql'));
-			while(! $file->eof()){
-				$line = $file->current();
-				if(substr($line, 0, 2) == '--' || substr($line, 0, 2) == '#' ||  $line == ''){ continue; }
-				
-				$line = str_replace('wp_', $wpdb->prefix, $line);
-				$query .= $line ;					
-				if(substr(trim($line), -1, 1) == ';'){
-					$wpdb->query($query);
-					$query = '';
-				}
-				$file->next();
-			}
-			$file = null ; 
-		}
-		static function migrate_drupal_post_types(){
-			
 		}
 
 	}
